e87e93969ef0ada6f844a9f1feb77f997fe1509b
Constantin Veziteu <constantin.veziteu@outlook.com>
2019-06-19 16:00:35 +0300

    Developer update #7
    1) Refactorized the script.js file (woop woop ^_^);
    2) Created Outlook icon;
    3) Added Outlook button to landing page.
    4) Added comments in index.html page.

--- js/script.js
+++ js/script.js
@@ -1,34 +1,57 @@
-function myChangeThemeFunction(clickedId) {
-  var myThemes = ["theme-default", "theme-image", "theme-teal", "theme-brown"];
-  var currentTheme = "";
+// All theme names go to this array:
+var myThemes = ["theme-default", "theme-image", "theme-teal", "theme-brown"];
 
-  if (clickedId == "themeDefault") {
-    currentTheme = "theme-default";
-  }
-  else if (clickedId == "themeTeal") {
-      currentTheme = "theme-teal";
-  }
-  else if (clickedId == "themeBrown") {
-      currentTheme = "theme-brown";
-  }
-  else if (clickedId == "themeImage") {
-      currentTheme = "theme-image";
-  }
-  console.log(currentTheme + " - requested!");
+// Ids of the <li> elements
+// Ids should be in the same order as theme names above!
+// E.g. "theme-default" from "myThemes" array should be on the same position with "themeDefault" in "myIds" array
+var myIds = ["themeDefault", "themeImage", "themeTeal", "themeBrown"];
 
-  // var x = document.getElementsByTagName("BODY")[0];
-  // alert(x);
-  if (!document.getElementsByTagName("body")[0].classList.contains(currentTheme) ||
-      !document.getElementsByTagName("span")[0].classList.contains(currentTheme) ||
-      !document.getElementsByTagName("img")[0].classList.contains(currentTheme)) {
+// All html elements that are affected by the theme change
+var myElements = ["body", "span", "img"];
 
-        for(var i = 0; i <= myThemes.length; i++) {
-          document.getElementsByTagName("body")[0].classList.remove(myThemes[i]);
-          document.getElementsByTagName("span")[0].classList.remove(myThemes[i]);
-          document.getElementsByTagName("img")[0].classList.remove(myThemes[i])
-        }
-        document.getElementsByTagName("body")[0].classList.add(currentTheme);
-        document.getElementsByTagName("span")[0].classList.add(currentTheme);
-        document.getElementsByTagName("img")[0].classList.add(currentTheme);
+// Initialization of the selected theme variable.
+var selectedTheme = "";
+
+
+// Check if the theme is already applied to all elements.
+function checkElementsTheme(selectedTheme) {
+  for (var i = 0; i < myElements.length; i++) {
+    if (!document.getElementsByTagName(myElements[i])[0].classList.contains(selectedTheme)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Remove all "theme" classes from the elements.
+function removeClasses() {
+  for (var i = 0; i < myThemes.length; i++) {
+    for (var j = 0; j < myElements.length; j++) {
+      document.getElementsByTagName(myElements[j])[0].classList.remove(myThemes[i]);
+    }
+  }
+}
+
+// Add the theme class to all elements.
+function addClasses(selectedTheme) {
+  for (var j = 0; j < myElements.length; j++) {
+    document.getElementsByTagName(myElements[j])[0].classList.add(selectedTheme);
+  }
+}
+
+
+// Main function
+function ChangeTheme(clickedId) {
+
+  for (var i = 0; i < myThemes.length; i++) {
+    if (clickedId ==  myIds[i]) {
+      selectedTheme = myThemes[i];
+    }
+  }
+
+  if (!checkElementsTheme(selectedTheme)) {
+      console.log(selectedTheme + " - requested!");
+      removeClasses();
+      addClasses(selectedTheme);
   }
 }
